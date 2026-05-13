e27cbbb41856cf2eb740995ba0b9e2f0a7d10557
Aurus Soares <neiven131@gmail.com>
2026-05-13 02:39:04 -0300

    resolve agentService conflict

--- services/agentService.js
+++ services/agentService.js
@@ -186,14 +186,7 @@ async function registrarPausa(
         reject(err);
 
       } else {
-<<<<<<< HEAD
-
-        console.log(
-          `[STATUS] ${agente_nome} -> ${status}`
-        );
-
-=======
->>>>>>> d44ba0a2731b7f0bc046ce31cb2e0f5cfa028ac8
+ d44ba0a2731b7f0bc046ce31cb2e0f5cfa028ac8
         resolve(true);
 
       }
